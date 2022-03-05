70d601487ab57dca6495a7fd93387618a7b3e569
yangxiaofang <13588711374@139.com>
2022-03-05 20:29:58 +0800

    乾坤微应用首次提交

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useCallback, useEffect } from 'react';
+import './public-path';
+import {Routes, Link, Route } from 'react-router-dom';
 import './App.css';
 
-function App() {
+const Index = function() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>子项目首页</div>
+  )
+}
+const List = function() {
+  return (
+    <div>子项目列表页</div>
+  )
+}
+function App(props) {
+  useEffect(() => {
+
+    props.onGlobalStateChange((state, prev) => {
+      // state: 变更后的状态; prev 变更前的状态
+      console.log(state, prev, 2000000);
+    }, true);
+  
+    // props.setGlobalState({ name: '小锅巴'});
+  }, []);
+  return (
+    <div className="layout">
+      <div className="left">
+        <Link to="/childindex">子项目首页</Link>
+        <Link to="/childindexlist">子项目列表</Link>
+      </div>
+      <div className="content">
+        <Routes>
+            <Route path='/childindex' element={<Index />} />
+            <Route path='/childindexlist' element={<List />} />
+        </Routes>
+      </div>
     </div>
   );
 }
